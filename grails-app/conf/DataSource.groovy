b14993fec4aa2b71775552eeb5d313450c363266
student <student@WebArch.localdomain>
2013-05-08 18:47:10 +0100

    Created first domain class, domain and test directory.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -16,8 +16,8 @@ environments {
             dbCreate = "update"
             driverClassName = "com.mysql.jdbc.Driver"
             dialect=org.hibernate.dialect.MySQL5Dialect
-            username = "student"
-            password = "student"
+            username = "webarch"
+            password = "webarch"
             url = "jdbc:mysql://localhost/eregister?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
             pooled = true
         }
